dae93dc40aa290060b83e9893a7b31dad9475d6d
Khush0031 <chaurasiakhush0031@gmail.com>
2024-05-31 20:10:03 +0530

    .

---
+++ 09-Drop-Down/countries.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} countries
+ */
+export function getDropdown(countries) {
+    console.log(countries);
+    let html = "<option value=''>Please select</option>";
+    countries.forEach(function(country){
+         html += `<option value=${country.toLowerCase()}>${country}</option>`
+    })
+    console.log(html);
+    return html;
+}

5fa17e866cf71c533a723bb620a89fabdf086559
Khush Chaurasia <121889921+Khush0031@users.noreply.github.com>
2024-05-31 20:30:32 +0530

    Delete 09-Drop-Down directory

--- 09-Drop-Down/script.js
+++
@@ -1,8 +0,0 @@
-import {getDropdown} from "./countries.js";
-
-const countries = ["Netherlands", "Japan", "Mongolia"];
-
-const html = getDropdown(countries);
-
-const select = document.querySelector("#countries-dropdown");
-select.innerHTML = html;
